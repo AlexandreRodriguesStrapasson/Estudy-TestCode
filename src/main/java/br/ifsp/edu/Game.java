8c2375ea7c2fb4293d86f8dd5f428f46aa81df15
AlexandreStrapasson <alexandre.strapasson1@gmail.com>
2023-09-14 15:07:39 -0300

    Fix: commit a forgotten file

---
+++ src/main/java/br/ifsp/edu/Game.java
@@ -0,0 +1,12 @@
+package br.ifsp.edu;
+
+public class Game {
+    private int score;
+    public void roll(int pins) {
+        score += pins;
+    }
+
+    public int score() {
+        return score;
+    }
+}
---
+++ src/test/java/br/ifsp/edu/GameTest.java
@@ -0,0 +1,37 @@
+package br.ifsp.edu;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class GameTest {
+    @BeforeEach
+    void setUp(){
+
+    }
+
+    @Test
+    @DisplayName("Should score zero in gutter game")
+    void shouldScoreZeroInGutterGame() {
+        Game game = new Game();
+        for (int i = 0; i < 20; i++) {
+            game.roll(0);
+        }
+
+        assertThat(game.score()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("Should score 40 if scores always 2")
+    void shouldScore40IfScoresAlways2() {
+         Game game = new Game();
+        for (int i = 0; i < 20; i++) {
+            game.roll(2);
+        }
+
+        assertThat(game.score()).isEqualTo(40);
+    }
+
+
+}
